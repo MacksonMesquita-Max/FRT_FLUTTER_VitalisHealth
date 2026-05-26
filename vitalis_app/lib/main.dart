b41590c163d1ddac4223df55ca4a0bf4ed574ae8
MacksonMesquita-Max <macksondar.mesquita17@gmail.com>
2026-05-26 17:05:20 -0300

    chore: Configurações inciais, build, nome e logo de app

--- vitalis_app/lib/main.dart
+++ vitalis_app/lib/main.dart
@@ -7,13 +7,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vitalis Health',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
       ),
     );
   }
